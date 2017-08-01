6ad9e5c7b83c76858835c951c9aa88ae5fee1b3b
voidism <zetanollyjs-cp@yahoo.com.tw>
2017-08-01 10:25:53 +0800

    Update Dictionary Add&Del.py

--- Dictionary Add&Del.py
+++ Dictionary Add&Del.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 from Tkinter import *
 import os
-import requests
+import sys
 import json
 
 
@@ -13,16 +13,8 @@ class GUIDemo(Frame):
         global ewb, ewords
 
         if not os.path.exists('./EnWordBase.json'):
-            web = requests.get(
-                "https://raw.githubusercontent.com/voidism/Modify-Input-Type-Automatically/master/EnWordBase.json")
-            dic = web.json()
-            for i in range(len(dic['words'])):
-                if not dic['words'][i].islower():
-                    dic['words'][i] = dic['words'][i].lower()
-            with open('EnWordBase.json', 'w') as file1:
-                json.dump(dic, file1)
-            file1.close()
-            print 'Dictionary Loaded!'
+            print 'EnWordBase.json not found!'
+            sys.exit()
 
         with open('EnWordBase.json', 'r') as file2:
             ewb = json.load(file2)
